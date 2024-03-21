78835b6ad168dcfe1f3ea61129c5d6fa5f8ba773
FrogsTop1 <123440247+FrogsTop1@users.noreply.github.com>
2024-03-21 16:20:34 +0300

    Add files via upload

---
+++ bot_config.py
@@ -0,0 +1,24 @@
+nickname = ""
+portal = "yota"
+
+auto_chat_file = "autochat/auto_chat.txt"
+
+portal_numbers = {
+    "epsilon": 10,
+    "yota": 11,
+    "delta": 12,
+    "sigma": 13,
+    "alpha": 14,
+    "beta": 15,
+    "gamma": 16,
+    "omega": 21,
+    "hydra": 22,
+}
+
+auto_chat = False
+auto_chat_data = {}
+
+auto_clan_inviting = False
+auto_chat_game = False
+
+repetition_target = None
